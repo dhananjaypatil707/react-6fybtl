f26c3591be4b4c633d38dfde123f6b78eb70597f
dhananjaypatil707 <dhananjaypatil707@gmail.com>
2021-03-03 18:35:48 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-export default function App(props) {
+function App(props) {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
@@ -9,3 +9,5 @@ export default function App(props) {
     </div>
   );
 }
+
+export default App;
